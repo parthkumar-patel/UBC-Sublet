8fefc76cf8c7b6ee20583bdb80765d1c1c8159ee
Parth <parthp15@yahoo.com>
2024-02-20 17:08:08 -0800

    cleaned the code

--- UBC-Sublet/src/App.jsx
+++ UBC-Sublet/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar';
 import './App.css'
 
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
+import { getAuth, createUserWithEmailAndPassword, 
+  // signOut, signInWithEmailAndPassword,
+} from "firebase/auth"
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -20,52 +20,28 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 const auth = getAuth();
 
-// const signupForm = document.querySelector('.signup');
-// signupForm.addEventListener('submit', (e) => {
-//   e.preventDefault()
+const signupForm = document.querySelector('.signup');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-//   const email = signupForm.email.value
-//   const password = signupForm.password.value
+  const email = signupForm.email.value
+  const password = signupForm.password.value
 
-//   createUserWithEmailAndPassword(auth, email, password)
-//   .then((cred) => {
-//     // console.log('user created: ',cred.user)
-//     signupForm.reset()
-//   })
-//   .catch((err) => {
-//     console.log(err.mes)
-//   })
-// })
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((cred) => {
+    console.log('user created: ',cred.user)
+    signupForm.reset()
+  })
+  .catch((err) => {
+    console.log(err.mes)
+  })
+})
 
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app--container"> 
+          <Navbar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
-
-export default App
--- UBC-Sublet/src/components/navbar.jsx
+++ UBC-Sublet/src/components/navbar.jsx
@@ -1,7 +1,25 @@
-const navbar = () => {
+export default function Navbar() {
     return (
-        <div>navbar</div>
-    )
-}
+        <nav>
+            <h2>Firebase Auth</h2>
+  
+            <form className="signup">
+                <label htmlFor="email">email:</label> 
+                <input type="email" name="email" /> 
+                <label htmlFor="password">password:</label>
+                <input type="password" name="password" />
+                <button>signup</button>
+            </form>
 
-export default navbar
+            <form className="login">
+                <label htmlFor="email">email:</label>
+                <input type="email" name="email" />
+                <label htmlFor="password">password:</label>
+                <input type="password" name="password" />
+                <button>login</button>
+            </form>
+            
+            <button className="logout">logout</button>
+        </nav>
+    );
+}
