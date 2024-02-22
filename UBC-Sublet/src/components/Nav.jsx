f8cd6257866e3fe98ace871108015028f9566b6a
Parth <parthp15@yahoo.com>
2024-02-22 11:07:54 -0800

    navbar css updated

--- UBC-Sublet/src/components/Nav.jsx
+++ UBC-Sublet/src/components/Nav.jsx
@@ -6,22 +6,30 @@ export default function Navbar() {
 
     const handleSignOut = async () => {
         try {
-          await logOut()
+          await logOut();
         } catch (error) {
-          console.log(error)
+          console.log(error);
         }
-    }
+    };
 
     return (
-        <div className="d-flex justify-content-between bg-secondary w-100 p-4">
-            <h1 className="text-center text-2xl font-weight-bold text-white">
-                Firebase Google Auth & Context
-            </h1>
-            {user?.displayName ? (
-                <button onClick={handleSignOut} className='border py-2 px-5 mt-10'>Logout</button>
-            ) : (
-                <Link to='/signin'>Sign in</Link>
-            )}
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-secondary fixed-top">
+            <div className="container">
+                <Link to="/" className="navbar-brand">Firebase Google Auth & Context</Link>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav ml-auto">
+                        {user?.displayName ? (
+                            <li className="nav-item">
+                                <button onClick={handleSignOut} className="btn btn-outline-light">Logout</button>
+                            </li>
+                        ) : (
+                            <li className="nav-item">
+                                <Link to="/signin" className="btn btn-outline-light">Sign in</Link>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
