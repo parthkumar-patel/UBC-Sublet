4bf00bbc87d9c3292263a805231e6e8421406255
Rshah2004 <rythemshah2004@gmail.com>
2024-03-06 13:55:18 -0800

    4 outta 10 filters have been applied

--- UBC-Sublet/src/components/SearchSublet.jsx
+++ UBC-Sublet/src/components/SearchSublet.jsx
@@ -1,3 +1,4 @@
+import { isEmpty } from "@firebase/util";
 import React, { useState, useEffect } from "react";
 // import React from 'react';
 import { useLocation } from 'react-router-dom';
@@ -35,22 +36,98 @@ export default function SearchList() {
             }
         });
     },  [entireData]);
-
-    return ( 
-        <div className="featuredITems"> Featured sublets
-        {conditionalData.map((items, index) => (
-                <div key = {index} className="card">
-                {items.rooms.map((singleImage, imageIndex) => (
-                    <div key ={imageIndex} class = "img">
-                            <img src={singleImage} alt="pic" width="170px" />
+    if (conditionalData.length != 0) {
+        return ( 
+            <div className="featuredITems">
+                <h1 style = {{
+                marginTop: '-350px',
+                marginLeft: '210px',
+                fontSize: '40px',
+                fontWeight: 'bold',
+                fontFamily: 'Archivo Black'
+            }}> Sublets in UBC</h1>
+            
+            {conditionalData.map((items, index) => (
+                    <div key = {index} className="card"
+                    style={{
+                        marginLeft: '200px'
+                    }}>
+                    {items.rooms.map((singleImage, imageIndex) => (
+                        <div key ={imageIndex} class = "img">
+                            <img src={singleImage} alt="pic" width="170px"/>
+                        </div>
+                    ))} 
+                    <div className="card--stats">
+                        <div class = "firstElement"> <h6> {items.roomType} </h6> </div>
+                        <div> {items.location.currentLocation} </div>
                     </div>
-                ))} 
-                <div className="card--stats">
-                    <div class = "firstElement"> <h6> {items.roomType} </h6> </div>
-                    <div> {items.location.currentLocation} </div>
-                </div>
-            </div>                            
-        ))};
+                </div>                            
+            ))}
+            <div>
+                <h2 style = {{ marginLeft: '-175px', marginTop: '-335px', position: 'aboslute', fontSize: '30px', fontWeight: 'bold', fontFamily: 'Archivo Black'}}> Filters </h2>
+                <button type="button" class="btn btn-outline-secondary"
+                style = {{
+                    marginLeft: '-200px',
+                    marginTop: '20px'
+                }}> Newest </button>
+
+                <button type="button" class="btn btn-outline-secondary"
+                style = {{
+                    marginLeft: '10px',
+                    marginTop: '20px',
+                    position: 'absolute'
+                }}> Oldest </button>
+
+                <button type="button" class="btn btn-outline-secondary"
+                style = {{
+                    marginLeft: '-80px',
+                    marginTop: '70px',
+                    position: 'absolute'
+                }}> Lowest Price </button>
+
+                <button type="button" class="btn btn-outline-secondary"
+                style = {{
+                    marginLeft: '50px',
+                    marginTop: '70px',
+                    position: 'absolute'
+                }}> Highest Price </button>
+                 
+                <button type="button" class="btn btn-outline-secondary"
+                style = {{
+                    marginLeft: '-80px',
+                    marginTop: '120px',
+                    position: 'absolute'
+                }}> Shortest Lease </button>
+
+                <button type="button" class="btn btn-outline-secondary"
+                style = {{
+                    marginLeft: '-80px',
+                    marginTop: '170px',
+                    position: 'absolute'
+                }}> Longest Lease </button>
+                <label htmlFor="customRange2" class="form-label"
+                style = {{
+                    marginTop: '230px',
+                    position: 'absolute',
+                    marginLeft: '-80px',
+                    fontSize: '15px'
+                }}> Price  </label>
+                <input type="range" class="form-range" min= '0'
+                    max= '1000' id="customRange2"
+                style = {{
+                    maxInlineSize: '300px',
+                    marginTop: '255px',
+                    marginLeft: '-80px',
+                    position: 'absolute',
+                }}/>
+        
+            </div>
+            </div>
+        )
+    } else {
+    return (
+        <div>
+            <h1> No search results found </h1>
         </div>
-    )
+    )}
 }
