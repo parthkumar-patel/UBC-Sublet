62af0f50ce181d6b420d34a039305c3062649a90
Parth <parthp15@yahoo.com>
2024-03-11 05:17:54 -0700

    cleaned up the code

--- UBC-Sublet/src/pages/Home.jsx
+++ UBC-Sublet/src/pages/Home.jsx
@@ -1,15 +1,11 @@
 import Search from "../components/Search";
 import Card from "../components/Card";
-// import MeetingCard from "../components/MeetingCard";
-// import YourCardComponent from "../components/YourCardComponent";
 
 export default function Home() {
 	return (
 		<div className="text-center">
 			<Search />
 			<Card />
-			{/* <MeetingCard /> */}
-			{/* <YourCardComponent /> */}
 		</div>
 	)
 }
