db63b9f6734e063ce0b9c10bb37659a459742545
Parth <parthp15@yahoo.com>
2024-03-31 11:46:44 -0700

    removed unecessary css

--- UBC-Sublet/src/components/Description.jsx
+++ UBC-Sublet/src/components/Description.jsx
@@ -1,6 +1,6 @@
 import "./styles/desc.css";
 import Contact from "./Contact";
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useLocation } from "react-router-dom";
 
 
