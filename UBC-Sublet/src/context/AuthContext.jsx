e6c7868c0df46e59283ede4d2f604e0b07fa1178
Rshah2004 <rythemshah2004@gmail.com>
2024-04-02 01:52:03 -0700

    description fixed

--- UBC-Sublet/src/context/AuthContext.jsx
+++ UBC-Sublet/src/context/AuthContext.jsx
@@ -11,25 +11,23 @@ import {
 import PropTypes from "prop-types";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyABsui21YwsnUrrzZZMEFc4z_BBINYcCPA",
+  authDomain: "ubc-sublet.firebaseapp.com",
+  projectId: "ubc-sublet",
+  storageBucket: "ubc-sublet.appspot.com",
+  messagingSenderId: "744862491087",
+  appId: "1:744862491087:web:a44f1fe890494086b772ba",
+  measurementId: "G-943F4K57XC",
+};
 
+initializeApp(firebaseConfig);
+const auth = getAuth();
+
+// google sign in
+const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const firebaseConfig = {
-    apiKey: "AIzaSyABsui21YwsnUrrzZZMEFc4z_BBINYcCPA",
-    authDomain: "ubc-sublet.firebaseapp.com",
-    projectId: "ubc-sublet",
-    storageBucket: "ubc-sublet.appspot.com",
-    messagingSenderId: "744862491087",
-    appId: "1:744862491087:web:a44f1fe890494086b772ba",
-    measurementId: "G-943F4K57XC",
-  };
-  
-  initializeApp(firebaseConfig);
-  const auth = getAuth();
-  
-  // google sign in
-  const AuthContext = createContext();
-
   const [user, setUser] = useState(null);
 
   const googleSignIn = () => {
@@ -62,24 +60,5 @@ AuthContextProvider.propTypes = {
 };
 
 export const UserAuth = () => {
-  AuthContextProvider.propTypes = {
-    children: PropTypes.node.isRequired,
-  };
-  
-  const firebaseConfig = {
-    apiKey: "AIzaSyABsui21YwsnUrrzZZMEFc4z_BBINYcCPA",
-    authDomain: "ubc-sublet.firebaseapp.com",
-    projectId: "ubc-sublet",
-    storageBucket: "ubc-sublet.appspot.com",
-    messagingSenderId: "744862491087",
-    appId: "1:744862491087:web:a44f1fe890494086b772ba",
-    measurementId: "G-943F4K57XC",
-  };
-  
-  initializeApp(firebaseConfig);
-  const auth = getAuth();
-  
-  // google sign in
-  const AuthContext = createContext();
   return useContext(AuthContext);
 };
