f6a38b657f154d86f1004858c98c348abc9ab73a
Rshah2004 <rythemshah2004@gmail.com>
2024-04-01 23:34:05 -0700

    zindexing

--- UBC-Sublet/src/main.jsx
+++ UBC-Sublet/src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 // Check for production environment
 if (process.env.NODE_ENV === "production") {
