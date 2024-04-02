b84536d2ea312a1cd06c6838fc43cecc1f121792
Rshah2004 <rythemshah2004@gmail.com>
2024-04-02 01:51:31 -0700

    description fixed

--- UBC-Sublet/src/context/AuthContext.jsx
+++ UBC-Sublet/src/context/AuthContext.jsx
@@ -29,6 +29,7 @@ export const AuthContextProvider = ({ children }) => {
   
   // google sign in
   const AuthContext = createContext();
+
   const [user, setUser] = useState(null);
 
   const googleSignIn = () => {
@@ -61,5 +62,24 @@ AuthContextProvider.propTypes = {
 };
 
 export const UserAuth = () => {
+  AuthContextProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyABsui21YwsnUrrzZZMEFc4z_BBINYcCPA",
+    authDomain: "ubc-sublet.firebaseapp.com",
+    projectId: "ubc-sublet",
+    storageBucket: "ubc-sublet.appspot.com",
+    messagingSenderId: "744862491087",
+    appId: "1:744862491087:web:a44f1fe890494086b772ba",
+    measurementId: "G-943F4K57XC",
+  };
+  
+  initializeApp(firebaseConfig);
+  const auth = getAuth();
+  
+  // google sign in
+  const AuthContext = createContext();
   return useContext(AuthContext);
 };
